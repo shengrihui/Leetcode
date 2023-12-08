65a26af0882c6ed5288bcc09a6db7ad328e500b4
shengrihui <1120058252@qq.com>
2023-12-08 23:27:05 +0800

    imports.py

--- Temp/leetcode/editor/cn/imports.py
+++ Temp/leetcode/editor/cn/imports.py
@@ -1,8 +1,15 @@
+from typing import Optional, List
+from itertools import *
+from collections import *
+from functools import *
+from bisect import *
+from math import inf
+import heapq
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
