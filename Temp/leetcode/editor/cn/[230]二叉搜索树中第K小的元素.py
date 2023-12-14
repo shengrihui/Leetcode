7eb275804bf135dd3e8a61df7db1307c9020573e
shengrihui <1120058252@qq.com>
2023-12-14 18:21:10 +0800

    灵神基础算法精讲11 第2次

--- Temp/leetcode/editor/cn/[230]二叉搜索树中第K小的元素.py
+++ Temp/leetcode/editor/cn/[230]二叉搜索树中第K小的元素.py
@@ -11,21 +11,55 @@ from imports import *
 #         self.left = left
 #         self.right = right
 class Solution:
+    # 递归
+    # def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+    #     cnt = 0
+    #
+    #     def mid(root: Optional[TreeNode]) -> int:
+    #         if not root:
+    #             return inf
+    #         l = mid(root.left)
+    #         if l != inf:
+    #             return l
+    #         nonlocal cnt
+    #         # 当前节点是第 cnt 个
+    #         cnt += 1
+    #         if cnt == k:
+    #             return root.val
+    #         return mid(root.right)
+    #
+    #     return mid(root)
+
+    # 迭代
+    # 向左之前将当前 root 入栈
+    # def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+    #     st = []
+    #     while root or st:
+    #         while root:
+    #             st.append(root)
+    #             root = root.left
+    #         k -= 1
+    #         root = st.pop()
+    #         if k == 0:
+    #             return root.val
+    #         root = root.right
+
+    # Morris
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        cnt = 0
-
-        def mid(root: Optional[TreeNode]) -> int:
-            if not root:
-                return inf
-            l = mid(root.left)
-            if l != inf:
-                return l
-            nonlocal cnt
-            cnt += 1
-            if cnt == k:
+        while root:
+            if root.left:
+                predecessor = root.left
+                while predecessor.right and predecessor.right != root:
+                    predecessor = predecessor.right
+                if predecessor.right == root:
+                    predecessor.right = None
+                else:
+                    predecessor.right = root
+                    root = root.left
+                    continue
+            k -= 1
+            if k == 0:
                 return root.val
-            return mid(root.right)
-
-        return mid(root)
+            root = root.right
 
 # leetcode submit region end(Prohibit modification and deletion)
--- Temp/leetcode/editor/cn/[501]二叉搜索树中的众数.py
+++ Temp/leetcode/editor/cn/[501]二叉搜索树中的众数.py
@@ -11,33 +11,82 @@ from imports import *
 #         self.left = left
 #         self.right = right
 class Solution:
+    # 递归
+    # def findMode(self, root: Optional[TreeNode]) -> List[int]:
+    #     ans = []
+    #     pre, cnt, mx_cnt = -inf, 0, 0
+    #
+    #     def mid(root: Optional[TreeNode]) -> None:
+    #         nonlocal ans, pre, cnt, mx_cnt
+    #         if not root:
+    #             return None
+    #         mid(root.left)
+    #         x = root.val
+    #         if x == pre:
+    #             cnt += 1
+    #         else:
+    #             if cnt > mx_cnt:
+    #                 mx_cnt = cnt
+    #                 ans = [pre]
+    #             elif cnt == mx_cnt:
+    #                 ans.append(pre)
+    #             cnt = 1
+    #             pre = x
+    #         mid(root.right)
+    #
+    #     mid(root)
+    #     if cnt > mx_cnt:
+    #         ans = [pre]
+    #     elif cnt == mx_cnt:
+    #         ans.append(pre)
+    #     return ans
+
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
         ans = []
-        pre, cnt, mx_cnt = -inf, 0, 0
+        pre, cnt, mx_cnt = 0, 0, 0
 
-        def mid(root: Optional[TreeNode]) -> None:
-            nonlocal ans, pre, cnt, mx_cnt
-            if not root:
-                return None
-            mid(root.left)
-            x = root.val
+        # update 和上面方法的区别：
+        # 在 cnt 与 mx_cmt 比较的时候，上一个方法的 cnt 代表的是 pre 的数量
+        # 而 update 方法代表的就是当前 x 的数量
+        # 因此在最后不用另外再来一遍
+        def update(x: int) -> None:
+            nonlocal pre, ans, cnt, mx_cnt
             if x == pre:
                 cnt += 1
             else:
-                if cnt > mx_cnt:
-                    mx_cnt = cnt
-                    ans = [pre]
-                elif cnt == mx_cnt:
-                    ans.append(pre)
                 cnt = 1
                 pre = x
-            mid(root.right)
+            if cnt == mx_cnt:
+                ans.append(x)
+            elif cnt > mx_cnt:
+                mx_cnt = cnt
+                ans = [x]
 
-        mid(root)
-        if cnt > mx_cnt:
-            ans = [pre]
-        elif cnt == mx_cnt:
-            ans.append(pre)
+        # Morris
+        while root:
+            if root.left:
+                predecessor = root.left
+                while predecessor.right and predecessor.right != root:
+                    predecessor = predecessor.right
+                if predecessor.right == root:
+                    predecessor.right = None
+                else:
+                    predecessor.right = root
+                    root = root.left
+                    continue
+            update(root.val)
+            root = root.right
+
+        # 迭代
+        """
+        st = []
+        while st or root:
+            while root:
+                st.append(root)
+                root = root.left
+            root = st.pop()
+            update(root.val)
+            root = root.right
+        """
         return ans
-
 # leetcode submit region end(Prohibit modification and deletion)
