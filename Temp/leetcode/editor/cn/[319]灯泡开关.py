90c9376395c941ee2f5c6617defcc72370076ef7
shengrihui <1120058252@qq.com>
2023-12-18 14:16:47 +0800

    20231218 319 灯泡开关

---
+++ Temp/leetcode/editor/cn/[319]灯泡开关.py
@@ -0,0 +1,12 @@
+# 319 灯泡开关
+# https://leetcode.cn/problems/bulb-switcher/
+import math
+
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return math.isqrt(n)
+# leetcode submit region end(Prohibit modification and deletion)
