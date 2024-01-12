361b960c2b4824eb409c9f22eb22a56156381860
shengrihui <1120058252@qq.com>
2024-01-13 07:51:32 +0800

    20240113 2182 构造限制重复的字符串 第 121 场双周赛修改

---
+++ Temp/leetcode/editor/cn/[2182]构造限制重复的字符串.py
@@ -0,0 +1,32 @@
+# 2182 构造限制重复的字符串
+# https://leetcode.cn/problems/construct-string-with-repeat-limit/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
+        cnt = [0] * 26
+        for c in s:
+            cnt[ord(c) - 97] += 1
+        ret = []
+        i, j, m = 25, 24, 0  # i 当前能填充的，j 下一个能填充的也就是当 i 达到限制的时候填充的
+        while i >= 0 and j >= 0:
+            if cnt[i] > 0 and m < repeatLimit:  # i 还能继续填充
+                ret.append(chr(97 + i))
+                cnt[i] -= 1
+                m += 1
+            elif j >= i or cnt[j] <= 0:  # j 不能填充，一直找到合适的 j
+                j -= 1
+            elif cnt[i] <= 0:
+                i = j  # 能走到这 elif， cnt[j] 一定 >0，直接让 i=j，
+                # j不用动，因为 while 条件是 and ，j 动了可能会到 -1 直接退出而此时 i 可能还能填充
+                ret.append(chr(97 + i))
+                cnt[i] -= 1
+                m = 1
+            elif m == repeatLimit:
+                ret.append(chr(97 + j))
+                cnt[j] -= 1
+                m = 0
+        return "".join(ret)
+# leetcode submit region end(Prohibit modification and deletion)
--- 周赛/2024/双周赛/第 121 场双周赛/3-100159. 使 X 和 Y 相等的最少操作次数.py
+++ 周赛/2024/双周赛/第 121 场双周赛/3-100159. 使 X 和 Y 相等的最少操作次数.py
@@ -1,4 +1,5 @@
-from collections import *
+from functools import cache
+from collections import deque
 
 
 # 题目：100159. 使 X 和 Y 相等的最少操作次数
@@ -6,6 +7,8 @@ from collections import *
 # 竞赛：https://leetcode.cn/contest/biweekly-contest-121/problems/minimum-number-of-operations-to-make-x-and-y-equal/
 # 题库：https://leetcode.cn/problems/minimum-number-of-operations-to-make-x-and-y-equal
 
+# BFS
+"""
 class Solution:
     def minimumOperationsToMakeEqual(self, x: int, y: int) -> int:
         if x <= y:
@@ -32,6 +35,23 @@ class Solution:
                         vis.add(nx)
 
             step += 1
+"""
+
+# 记忆化搜索
+# 时间复杂度： O((logx)^2)
+class Solution:
+    @cache
+    def minimumOperationsToMakeEqual(self, x: int, y: int) -> int:
+        if x <= y:
+            return y - x
+        return min(x - y,  # 全部采用 -1 操作
+                   # 将 x 向下变成 11 的倍数，x%11 x多少次变成11的倍数，+1 //11这一次的操作
+                   self.minimumOperationsToMakeEqual(x // 11, y) + x % 11 + 1,
+                   # 将 x 向上变成 11 的倍数
+                   self.minimumOperationsToMakeEqual(x // 11 + 1, y) + 11 - x % 11 + 1,
+                   self.minimumOperationsToMakeEqual(x // 5, y) + x % 5 + 1,
+                   self.minimumOperationsToMakeEqual(x // 5 + 1, y) + +5 - x % 5 + 1,
+                   )
 
 
 s = Solution()
@@ -39,6 +59,7 @@ examples = [
     (dict(x=26, y=1), 3),
     (dict(x=54, y=2), 4),
     (dict(x=25, y=30), 5),
+    (dict(x=10 ** 18, y=30), -1),
 ]
 for e, a in examples:
     print(a, e)
