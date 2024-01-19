b2ddcd078742b4492b487df5b510af9b39b2d34e
shengrihui <1120058252@qq.com>
2024-01-20 00:13:07 +0800

    20240120 2788 按分隔符拆分字符串

---
+++ Temp/leetcode/editor/cn/[2788]按分隔符拆分字符串.py
@@ -0,0 +1,17 @@
+# 2788 按分隔符拆分字符串
+# https://leetcode.cn/problems/split-strings-by-separator/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
+        # ans = []
+        # for w in words:
+        #     for n in w.split(separator):
+        #         if n:
+        #             ans.append(n)
+        # return ans
+        return [ww for w in words for ww in w.split(separator) if ww]
+
+# leetcode submit region end(Prohibit modification and deletion)
