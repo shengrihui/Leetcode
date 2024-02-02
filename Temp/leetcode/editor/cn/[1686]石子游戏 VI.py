704a86f3822f71fd1bf86cc96a6d640ace3dac98
shengrihui <1120058252@qq.com>
2024-02-02 10:11:04 +0800

    20240202 1686 石子游戏 VI

---
+++ Temp/leetcode/editor/cn/[1686]石子游戏 VI.py
@@ -0,0 +1,15 @@
+# 1686 石子游戏 VI
+# https://leetcode.cn/problems/stone-game-vi/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def stoneGameVI(self, aliceValues: List[int], bobValues: List[int]) -> int:
+        # v = sorted([(a + b, i) for i, (a, b) in enumerate(zip(aliceValues, bobValues))], reverse=True)
+        # a, b = sum(aliceValues[i] for va, i in v[::2]), sum(bobValues[i] for va, i in v[1::2])
+        # return 1 if a > b else (0 if a == b else -1)
+        v = sorted([a + b for a, b in zip(aliceValues, bobValues)], reverse=True)
+        ans = sum(v[::2]) - sum(bobValues)  # alice 和 bob 最后的价值差
+        return 1 if ans > 0 else (0 if ans == 0 else -1)
+# leetcode submit region end(Prohibit modification and deletion)
