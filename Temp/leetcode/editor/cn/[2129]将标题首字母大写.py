a5d66733d6b4ce4793fb18271b875460ef363e0c
shengrihui <1120058252@qq.com>
2024-03-11 09:27:36 +0800

    20240311 2129 将标题首字母大写

---
+++ Temp/leetcode/editor/cn/[2129]将标题首字母大写.py
@@ -0,0 +1,18 @@
+# 2129 将标题首字母大写
+# https://leetcode.cn/problems/capitalize-the-title/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        # ans = []
+        # for word in title.split():
+        #     if len(word) <= 2:
+        #         ans.append(word.lower())
+        #     else:
+        #         ans.append(word[0].upper() + word[1:].lower())
+        # return " ".join(ans)
+        return " ".join(
+            [word[0].upper() + word[1:].lower() if len(word) > 2 else word.lower() for word in title.split()])
+# leetcode submit region end(Prohibit modification and deletion)
