a805eb438831845ac3af6d692e665b869fa6b466
shengrihui <1120058252@qq.com>
2024-03-14 08:56:32 +0800

    20240314 2789 合并后数组中的最大元素

---
+++ Temp/leetcode/editor/cn/[2789]合并后数组中的最大元素.py
@@ -0,0 +1,14 @@
+# 2789 合并后数组中的最大元素
+# https://leetcode.cn/problems/largest-element-in-an-array-after-merge-operations/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def maxArrayValue(self, nums: List[int]) -> int:
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] <= nums[i + 1]:
+                nums[i] += nums[i + 1]
+        return nums[0]
+
+# leetcode submit region end(Prohibit modification and deletion)
