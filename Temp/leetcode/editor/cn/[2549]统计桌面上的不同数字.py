4045839da20f2019bed436918b034008a3934246
shengrihui <1120058252@qq.com>
2024-03-23 09:57:44 +0800

    每日一题 2549 统计桌面上的不同数字

---
+++ Temp/leetcode/editor/cn/[2549]统计桌面上的不同数字.py
@@ -0,0 +1,9 @@
+# 2549 统计桌面上的不同数字
+# https://leetcode.cn/problems/count-distinct-numbers-on-board/
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def distinctIntegers(self, n: int) -> int:
+        return n - 1 if n > 1 else 1
+# leetcode submit region end(Prohibit modification and deletion)
