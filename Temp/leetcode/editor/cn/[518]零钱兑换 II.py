5e445553a790d9f647df66018e4c97547640f236
shengrihui <1120058252@qq.com>
2024-03-25 15:33:37 +0800

    每日一题 518 零钱兑换 II

---
+++ Temp/leetcode/editor/cn/[518]零钱兑换 II.py
@@ -0,0 +1,15 @@
+# 518 零钱兑换 II
+# https://leetcode.cn/problems/coin-change-ii/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for c in coins:
+            for t in range(c, amount + 1):
+                dp[t] += dp[t - c]
+        return dp[amount]
+# leetcode submit region end(Prohibit modification and deletion)
