c8e937ed5636ceba43b85c12d55342db8f14ca5b
shengrihui <1120058252@qq.com>
2024-04-01 11:29:08 +0800

    每日一题 2810 故障键盘

---
+++ Temp/leetcode/editor/cn/[2810]故障键盘.py
@@ -0,0 +1,30 @@
+# 2810 故障键盘
+# https://leetcode.cn/problems/faulty-keyboard/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+
+class Solution:
+    def finalString(self, s: str) -> str:
+        q = deque()
+        tail = True
+        for c in s:
+            if c == "i":
+                tail = not tail
+            elif tail:
+                q.append(c)
+            else:
+                q.appendleft(c)
+        return "".join(q if tail else reversed(q))
+
+# class Solution:
+#     def finalString(self, s: str) -> str:
+#         ans = ""
+#         for c in s:
+#             if c == "i":
+#                 ans = ans[::-1]
+#             else:
+#                 ans += c
+#         return ans
+# leetcode submit region end(Prohibit modification and deletion)
