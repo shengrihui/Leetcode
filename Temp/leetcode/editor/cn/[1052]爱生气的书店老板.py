d644c7d29acde00ed387123abc52102881401894
shengrihui <1120058252@qq.com>
2024-04-23 09:08:15 +0800

    每日一题

---
+++ Temp/leetcode/editor/cn/[1052]爱生气的书店老板.py
@@ -0,0 +1,23 @@
+# 1052 爱生气的书店老板
+# https://leetcode.cn/problems/grumpy-bookstore-owner/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        n = len(customers)
+        ans = 0
+        pre = [0] * (n + 1)
+        for i, (c, g) in enumerate(zip(customers, grumpy)):
+            if g == 0:
+                ans += c
+                pre[i + 1] = pre[i]
+            else:
+                pre[i + 1] = pre[i] + c
+        happy = 0
+        for right in range(minutes, n + 1):
+            happy = max(happy, pre[right] - pre[right - minutes])
+        return ans + happy
+
+# leetcode submit region end(Prohibit modification and deletion)
