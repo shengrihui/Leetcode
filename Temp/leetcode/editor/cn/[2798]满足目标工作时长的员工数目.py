5f3b7e24649c0761068f7da24df753682be9796b
shengrihui <1120058252@qq.com>
2024-04-30 08:47:28 +0800

    每日一题

---
+++ Temp/leetcode/editor/cn/[2798]满足目标工作时长的员工数目.py
@@ -0,0 +1,10 @@
+# 2798 满足目标工作时长的员工数目
+# https://leetcode.cn/problems/number-of-employees-who-met-the-target/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        return sum(h >= target for h in hours)
+# leetcode submit region end(Prohibit modification and deletion)
