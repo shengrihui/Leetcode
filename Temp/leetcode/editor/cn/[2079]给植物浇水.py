7b5d0bb413fb96d31c825ece416eaefbefca561a
shengrihui <1120058252@qq.com>
2024-05-08 08:30:58 +0800

    每日一题

---
+++ Temp/leetcode/editor/cn/[2079]给植物浇水.py
@@ -0,0 +1,18 @@
+# 2079 给植物浇水
+# https://leetcode.cn/problems/watering-plants/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        ans = 0
+        rest = capacity
+        for i, x in enumerate(plants):
+            if rest < x:
+                rest = capacity
+                ans += i * 2
+            ans += 1
+            rest -= x
+        return ans
+# leetcode submit region end(Prohibit modification and deletion)
