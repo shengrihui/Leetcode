59339fc247da2e87d5e7557c07f184e963175882
shengrihui <1120058252@qq.com>
2024-05-19 07:45:47 +0800

    每日一题

---
+++ Temp/leetcode/editor/cn/[1535]找出数组游戏的赢家.py
@@ -0,0 +1,18 @@
+# 1535 找出数组游戏的赢家
+# https://leetcode.cn/problems/find-the-winner-of-an-array-game/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def getWinner(self, arr: List[int], k: int) -> int:
+        win, mx = -1, arr[0]
+        for x in arr:
+            if x > mx:
+                mx, win = x, 0
+            win += 1
+            if win == k:
+                return mx
+        return mx
+
+# leetcode submit region end(Prohibit modification and deletion)
