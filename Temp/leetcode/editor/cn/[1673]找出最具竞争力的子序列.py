b37098356256ef73a08efb1bae2f2402a12c048a
shengrihui <1120058252@qq.com>
2024-05-24 12:28:50 +0800

    第 398 场周赛

---
+++ Temp/leetcode/editor/cn/[1673]找出最具竞争力的子序列.py
@@ -0,0 +1,16 @@
+# 1673 找出最具竞争力的子序列
+# https://leetcode.cn/problems/find-the-most-competitive-subsequence/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
+        st = []
+        for idx, x in enumerate(nums):
+            while st and st[-1] > x and len(st) + len(nums) - idx > k:
+                st.pop()
+            if len(st) < k:
+                st.append(x)
+        return st
+# leetcode submit region end(Prohibit modification and deletion)
