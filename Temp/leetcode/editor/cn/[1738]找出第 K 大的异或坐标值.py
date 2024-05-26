9c4153ded02cd3824178a6f4aee9c97ec7cff326
shengrihui <1120058252@qq.com>
2024-05-26 21:08:52 +0800

    第 398 场周赛

---
+++ Temp/leetcode/editor/cn/[1738]找出第 K 大的异或坐标值.py
@@ -0,0 +1,16 @@
+# 1738 找出第 K 大的异或坐标值
+# https://leetcode.cn/problems/find-kth-largest-xor-coordinate-value/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def kthLargestValue(self, matrix: List[List[int]], k: int) -> int:
+        m, n = len(matrix), len(matrix[0])
+        rows = [list(accumulate(row, lambda x, y: x ^ y)) for row in matrix]
+        for i in range(1, m):
+            for j in range(n):
+                rows[i][j] ^= rows[i - 1][j]
+        a = sorted([x for row in rows for x in row], reverse=True)
+        return a[k - 1]
+# leetcode submit region end(Prohibit modification and deletion)
