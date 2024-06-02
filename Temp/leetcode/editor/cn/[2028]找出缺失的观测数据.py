8944b384051cd9156b358e30fe41be97cc22765d
shengrihui <1120058252@qq.com>
2024-06-02 15:52:19 +0800

    每日一题

---
+++ Temp/leetcode/editor/cn/[2028]找出缺失的观测数据.py
@@ -0,0 +1,17 @@
+# 2028 找出缺失的观测数据
+# https://leetcode.cn/problems/find-missing-observations/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        r = mean * (len(rolls) + n) - sum(rolls)
+        if r < n or r > n * 6:
+            return []
+        ans = [r // n] * n
+        for i in range(r % n):
+            ans[i] += 1
+        return ans
+
+# leetcode submit region end(Prohibit modification and deletion)
