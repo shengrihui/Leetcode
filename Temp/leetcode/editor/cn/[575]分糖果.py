7ce30f21449cc3f56b5703bbc3f592cdbf2e37f0
shengrihui <1120058252@qq.com>
2024-06-02 10:15:17 +0800

    每日一题

---
+++ Temp/leetcode/editor/cn/[575]分糖果.py
@@ -0,0 +1,11 @@
+# 575 分糖果
+# https://leetcode.cn/problems/distribute-candies/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        nn, m = len(candyType) // 2, len(set(candyType))
+        return nn if m > nn else m
+# leetcode submit region end(Prohibit modification and deletion)
