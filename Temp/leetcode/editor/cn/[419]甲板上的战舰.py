0ffce7faa5c32511577f5051607e8833beac1fa2
shengrihui <1120058252@qq.com>
2024-06-13 10:52:57 +0800

    每日一题

---
+++ Temp/leetcode/editor/cn/[419]甲板上的战舰.py
@@ -0,0 +1,16 @@
+# 419 甲板上的战舰
+# https://leetcode.cn/problems/battleships-in-a-board/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        ans = 0
+        for i, row in enumerate(board):
+            for j, c in enumerate(row):
+                if c == "X" and (i == 0 or board[i - 1][j] != "X") and \
+                        (j == 0 or board[i][j - 1] != "X"):
+                    ans += 1
+        return ans
+# leetcode submit region end(Prohibit modification and deletion)
