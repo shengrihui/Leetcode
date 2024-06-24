dda6b4c099a146a94ff52e9d3cf223c611bebf89
shengrihui <1120058252@qq.com>
2024-06-24 09:26:35 +0800

    每日一题

---
+++ Temp/leetcode/editor/cn/[503]下一个更大元素 II.py
@@ -0,0 +1,17 @@
+# 503 下一个更大元素 II
+# https://leetcode.cn/problems/next-greater-element-ii/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        st = []  # 单调递减栈
+        ans = [-1] * n
+        for i in range(n * 2):
+            while st and nums[st[-1]] < nums[i % n]:  # nums[i%n] 比 nums[st[-1]] 大
+                ans[st.pop()] = nums[i % n]
+            st.append(i % n)
+        return ans
+# leetcode submit region end(Prohibit modification and deletion)
