b25a55179f3aca67dcac17d85545a43890129c4a
shengrihui <1120058252@qq.com>
2024-07-05 19:42:04 +0800

    每日一题

---
+++ Temp/leetcode/editor/cn/[3033]修改矩阵.py
@@ -0,0 +1,11 @@
+# 3033 修改矩阵
+# https://leetcode.cn/problems/modify-the-matrix/
+from imports import *
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def modifiedMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
+        mx = [max(col) for col in zip(*matrix)]
+        return [[mx[j] if x == -1 else x for j, x in enumerate(row)] for i, row in enumerate(matrix)]
+# leetcode submit region end(Prohibit modification and deletion)
